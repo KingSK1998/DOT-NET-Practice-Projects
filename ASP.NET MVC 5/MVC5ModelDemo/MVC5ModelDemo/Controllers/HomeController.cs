93002fcb37c0b51898dcb35e4b876dacec782095
Shivam Kashyap <shiva.kas1888@gmail.com>
2022-09-03 22:04:16 +0530

    URL: /Home/

---
+++ ASP.NET MVC 5/MVC5ModelDemo/MVC5ModelDemo/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC5ModelDemo.Models;
+
+namespace MVC5ModelDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: /Home/
+        public ActionResult Index()
+        {
+            Product product = Product.GetProduct();
+            return View(product);
+        }
+
+        // GET: /Home/Welcome/
+        public string Welcome()
+        {
+            return "This is the welcome action method...";
+        }
+    }
+}
---
+++ ASP.NET MVC 5/MVC5ModelDemo/MVC5ModelDemo/Models/Product.cs
@@ -0,0 +1,25 @@
+﻿namespace MVC5ModelDemo.Models
+{
+    public class Product
+    {
+        public int? ProductId { get; set; }
+        public string? ProductName { get; set; }
+        public int? OrderId { get; set; }
+        public int? Quantity { get; set; }
+
+        public Product() { }
+
+        public Product(int productId, string? productName, int orderId, int quantity)
+        {
+            ProductId = productId;
+            ProductName = productName;
+            OrderId = orderId;
+            Quantity = quantity;
+        }
+
+        public static Product GetProduct()
+        {
+            return new Product(01, "C# Book", 02, 2);
+        }
+    }
+}
